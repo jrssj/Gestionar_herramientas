a73c8b829d572fee04cd027ddd2720a9a766d8f0
jrssj <jr8465207@gmail.com>
2026-04-10 19:49:46 -0500

    feat: nuevas clases : actualizar y listar personas

--- gestionar_personas.py
+++ gestionar_personas.py
@@ -3,3 +3,9 @@ def registrar_persona():
 
 def eliminar_persona():
     print("Eliminar persona")
+
+def actualizar_persona():
+    print("Actualizar persona")
+    
+def listar_personas(): 
+    print("Listar personas")
