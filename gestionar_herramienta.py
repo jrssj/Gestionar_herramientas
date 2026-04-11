b70a1a44f53c95db01d8a1c5336261e38d70c854
jrssj <jr8465207@gmail.com>
2026-04-10 20:09:43 -0500

    feat nuevas funciones

---
+++ gestionar_herramienta.py
@@ -0,0 +1,10 @@
+def registrar_herramienta():
+    print("Registrar herramienta")
+def eliminar_herramienta():
+    print("Eliminar herramienta")
+def actualizar_herramienta():
+    print("Actualizar herramienta") 
+def listar_herramientas(): 
+    print("Listar herramientas")
+
+    
