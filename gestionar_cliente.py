6af1254d086bf4a6101764ed46c229617cb4580d
jrssj <jr8465207@gmail.com>
2026-04-10 20:02:36 -0500

    feat: nuevas clases

---
+++ gestionar_cliente.py
@@ -0,0 +1,8 @@
+def registrar_persona():
+    print("Registrar persona")
+def eliminar_persona():
+    print("Eliminar persona")
+def actualizar_persona():
+    print("Actualizar persona")
+def listar_personas():
+    print("Listar personas")
